290784e5e140cc0c50a7040f00fee8b59e1d1c35
Levy <edgarlevy81@gmail.com>
2023-10-26 23:42:06 +0300

    Lesson learnt

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name, age, breed) VALUES ('Lil Bub', 5, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+SELECT [names of columns we are going to select] FROM [table we are selecting from];
+SELECT * FROM cats;
+SELECT * FROM cats WHERE name = 'Maru';
